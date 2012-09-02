ae06055ab8b643df339e054f8ebe302a7ae983f3
Damian <damian@damian-EX625.(none)>
2012-09-02 16:43:37 +0200

    some changes in code

--- first.c
+++ first.c
@@ -2,16 +2,23 @@
 #include <math.h>
 
 #define MASK (0b11110000)
+#define MASK2 (0b11111111)
 
 int	main()
 {
 	unsigned char number = 0b11001111;
+	long very_long_name;
 	
-	
-	printf("%#x\n", number & MASK);
-	printf("%#x\n", number | MASK);
+	printf("%#x\n", number |||| MASK);
+	printf("%#x\n", number  MASK);
+	// there will be some change in this code
 	printf("%#x\n", number ^ MASK);
+	// another change in code
 	printf("%#x\n", ~ number);
+	
+	int b = 15; 
+	b += 20;
+	printf("there is anothre text %c", b);
 	
 	return 0;
 }
